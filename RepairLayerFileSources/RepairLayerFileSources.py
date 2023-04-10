02fa62ba9f23a553642b81dc99a1cfbfc98a9238
Cole Rosner <colerosner@gmail.com>
2023-04-10 08:26:27 -0600

    Defined as function, added conditional

--- RepairLayerFileSources/RepairLayerFileSources.py
+++ RepairLayerFileSources/RepairLayerFileSources.py
@@ -1,27 +1,33 @@
 # Libraries
 import os, arcpy
 
-# Get current directory
-directory = os.getcwd()
+# Function
+def repairSources():
 
-# Loop
-for filename in os.listdir(directory):
-    if filename.endswith(".lyrx",3,8):    
-        lyrPath = os.path.join(directory, filename)
-        print(lyrPath)
-        lyrFile = arcpy.mp.LayerFile(lyrPath)
-        for lyr in lyrFile.listLayers():
-            if lyr.supports("datasource"):
-                if lyr.isBroken:
-                    cp = lyr.connectionProperties
-                    cp['connection_info']['database'] = directory
-                    for tif in os.listdir(directory):
-                        if tif.startswith(filename[:3]) and tif.endswith(".tif"):
-                            tifName = tif
-                    cp['dataset'] = tifName
-                    print("updating connection")
-                    lyr.updateConnectionProperties(lyr.connectionProperties, cp)
-        lyrFile.save()
-        print("moving on")
-        
-print("done")
+    # Get current directory
+    directory = os.getcwd()
+
+    # Loop
+    for filename in os.listdir(directory):
+        if filename.endswith(".lyrx",3,8):    
+            lyrPath = os.path.join(directory, filename)
+            print(lyrPath)
+            lyrFile = arcpy.mp.LayerFile(lyrPath)
+            for lyr in lyrFile.listLayers():
+                if lyr.supports("datasource"):
+                    if lyr.isBroken:
+                        cp = lyr.connectionProperties
+                        cp['connection_info']['database'] = directory
+                        for tif in os.listdir(directory):
+                            if tif.startswith(filename[:3]) and tif.endswith(".tif"): # Consider change to matching strings
+                                tifName = tif
+                        cp['dataset'] = tifName
+                        print("updating connection")
+                        lyr.updateConnectionProperties(lyr.connectionProperties, cp)
+            lyrFile.save()
+            print("moving on")
+            
+    print("done")
+
+if __name__ == '__main__':
+    repairSources()
