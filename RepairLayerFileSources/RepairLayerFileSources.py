11b591c59dbf3882741d7dedcfdf709076c5d6ee
Cole Rosner <colerosner@gmail.com>
2023-04-11 09:47:39 -0600

    Removed directory, replaced with fire_folder param

--- RepairLayerFileSources/RepairLayerFileSources.py
+++ RepairLayerFileSources/RepairLayerFileSources.py
@@ -2,23 +2,20 @@
 import os, arcpy
 
 # Function
-def repairSources():
-
-    # Get current directory
-    directory = os.getcwd()
+def repairSources(fire_folder):
 
     # Loop
-    for filename in os.listdir(directory):
+    for filename in os.listdir(fire_folder):
         if filename.endswith(".lyrx",3,8):    
-            lyrPath = os.path.join(directory, filename) # Consider change to just os.path.abspath(filename)
+            lyrPath = os.path.join(fire_folder, filename) # Consider change to just os.path.abspath(filename)
             print(lyrPath)
             lyrFile = arcpy.mp.LayerFile(lyrPath)
             for lyr in lyrFile.listLayers():
                 if lyr.supports("datasource"):
                     if lyr.isBroken:
                         cp = lyr.connectionProperties
-                        cp['connection_info']['database'] = directory
-                        for tif in os.listdir(directory):
+                        cp['connection_info']['database'] = fire_folder
+                        for tif in os.listdir(fire_folder):
                             if tif.startswith(filename[:3]) and tif.endswith(".tif"): # Consider change to matching strings
                                 tifName = tif
                         cp['dataset'] = tifName
@@ -30,4 +27,7 @@ def repairSources():
     print("done")
 
 if __name__ == '__main__':
-    repairSources()
+    
+    fire_folder = arcpy.GetParameterAsText(0)
+    
+    repairSources(fire_folder)
