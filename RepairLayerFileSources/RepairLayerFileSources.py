a67eafc60d80907240aca9d99c2041ad9960b3e6
Cole Rosner <colerosner@gmail.com>
2023-04-12 09:51:13 -0600

    Replaced print statements with arcpy.AddMessage()

--- RepairLayerFileSources/RepairLayerFileSources.py
+++ RepairLayerFileSources/RepairLayerFileSources.py
@@ -8,7 +8,6 @@ def repairSources(fire_folder):
     for filename in os.listdir(fire_folder):
         if filename.endswith(".lyrx",3,8):    
             lyrPath = os.path.join(fire_folder, filename) # Consider change to just os.path.abspath(filename)
-            print(lyrPath)
             lyrFile = arcpy.mp.LayerFile(lyrPath)
             for lyr in lyrFile.listLayers():
                 if lyr.supports("datasource"):
@@ -19,12 +18,12 @@ def repairSources(fire_folder):
                             if tif.startswith(filename[:3]) and tif.endswith(".tif"): # Consider change to matching strings
                                 tifName = tif
                         cp['dataset'] = tifName
-                        print("updating connection")
+                        arcpy.AddMessage("Updating {}\'s connection properties.".format(filename))
                         lyr.updateConnectionProperties(lyr.connectionProperties, cp)
             lyrFile.save()
-            print("moving on")
+            arcpy.AddMessage("{} source repaired successfully. New source is {}.".format(filename, tifName))
             
-    print("done")
+    arcpy.AddMessage("Sources in {} repaired successfully.".format(fire_folder))
 
 if __name__ == '__main__':
     
