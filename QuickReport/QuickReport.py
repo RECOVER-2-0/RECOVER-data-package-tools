7e4de3173f72776a64502eb3e3612da3776cbc70
Cole Rosner <colerosner@gmail.com>
2023-04-20 15:44:47 -0600

    Stop tool if Pro version not 3.1 or above

--- QuickReport/QuickReport.py
+++ QuickReport/QuickReport.py
@@ -1,1 +1,18 @@
+# This tool only works in ArcGIS Pro version 3.1 or above.
+# Many of the methods accessed via the ArcGISProject class
+# are not available in earlier versions of ArcGIS Pro. 
+
 # Libraries
+import arcpy, os
+
+def createQuickReport():
+    arcGISProVersion = arcpy.GetInstallInfo()["Version"]
+    if arcGISProVersion.startswith("3.1"): # Needs to not use .startswith(). What happens when ArcGIS Pro rolls over to 4.x?
+    # do something
+        pass
+    else:
+        print(f"This tool only works in ArcGIS Pro 3.1 or above. Current version: {arcGISProVersion}. Please update ArcGIS Pro to use this tool.")
+        return
+
+if __name__ == '__main__':
+    createQuickReport()
