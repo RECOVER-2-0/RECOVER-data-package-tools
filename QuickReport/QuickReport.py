c0432043e9320aa057ecab959d0e74e5c5ed4478
Cole Rosner <colerosner@gmail.com>
2023-04-21 11:07:52 -0600

    Functionality to add basic map w/ Fire Perimeter

--- QuickReport/QuickReport.py
+++ QuickReport/QuickReport.py
@@ -8,8 +8,19 @@ import arcpy, os
 def createQuickReport():
     arcGISProVersion = arcpy.GetInstallInfo()["Version"]
     if arcGISProVersion.startswith("3.1"): # Needs to not use .startswith(). What happens when ArcGIS Pro rolls over to 4.x?
-    # do something
-        pass
+        print(f"Current version: {arcGISProVersion}")
+        
+        aprx = arcpy.mp.ArcGISProject("CURRENT")
+        qrMap = aprx.createMap("QuickReportMap", "Map")
+        fp = fr"C:\Users\coler\Documents\ISU\WFH\Fire_2022_ORWWF_000400\Fire.gdb\Fire_Perimeter" # Parameterize
+        
+        qrMap.addDataFromPath(fp)
+        
+        fpLyr = qrMap.listLayers()[-1] # Retrieves the very last layer added to the newly created map
+        qrMap.defaultCamera.setExtent(arcpy.Describe(fpLyr).extent) # Set extent to that of the last layer added
+        
+        
+        print("done")
     else:
         print(f"This tool only works in ArcGIS Pro 3.1 or above. Current version: {arcGISProVersion}. Please update ArcGIS Pro to use this tool.")
         return
